70e854639f7ecc15c7f84fd147ea8477f1374899
Rahul Shah <rsha256@berkeley.edu>
2022-09-11 19:22:13 -0700

    MLP policy.py

--- hw1/cs285/infrastructure/utils.py
+++ hw1/cs285/infrastructure/utils.py
@@ -60,8 +60,9 @@ def sample_trajectories(env, policy, min_timesteps_per_batch, max_path_length, r
     timesteps_this_batch = 0
     paths = []
     while timesteps_this_batch < min_timesteps_per_batch:
-
-        TODO
+        s = sample_trajectory(env, policy, max_path_length, render)
+        paths.append(s)
+        timesteps_this_batch += get_pathlength(s)
 
     return paths, timesteps_this_batch
 
@@ -74,7 +75,9 @@ def sample_n_trajectories(env, policy, ntraj, max_path_length, render=False):
     """
     paths = []
 
-    TODO
+    for i in range(ntraj):
+        s = sample_trajectory(env, policy, max_path_length, render)
+        paths.append(s)
 
     return paths
 
--- hw1/cs285/policies/MLP_policy.py
+++ hw1/cs285/policies/MLP_policy.py
@@ -81,11 +81,11 @@ class MLPPolicy(BasePolicy, nn.Module, metaclass=abc.ABCMeta):
             observation = obs[None]
 
         # TODO return the action that the policy prescribes
-        raise NotImplementedError
+        return ptu.to_numpy(self.forward(ptu.from_numpy(observation)).sample())
 
     # update/train this policy
     def update(self, observations, actions, **kwargs):
-        raise NotImplementedError
+        res = self._update(observations, actions, **kwargs)
 
     # This function defines the forward pass of the network.
     # You can return anything you want, but you should be able to differentiate
@@ -93,7 +93,13 @@ class MLPPolicy(BasePolicy, nn.Module, metaclass=abc.ABCMeta):
     # return more flexible objects, such as a
     # `torch.distributions.Distribution` object. It's up to you!
     def forward(self, observation: torch.FloatTensor) -> Any:
-        raise NotImplementedError
+        if self.discrete:
+            return distributions.Categorical(logits=self.logits_na(observation))
+        else:
+            return distributions.Normal(
+                loc=self.mean_net(observation),
+                scale=torch.exp(self.logstd)
+            )
 
 
 #####################################################
@@ -109,7 +115,7 @@ class MLPPolicySL(MLPPolicy):
             adv_n=None, acs_labels_na=None, qvals=None
     ):
         # TODO: update the policy and return the loss
-        loss = TODO
+        loss = self.loss(self.forward(ptu.from_numpy(observations)).sample(), ptu.from_numpy(actions))
         return {
             # You can add extra logging information here, but keep this line
             'Training Loss': ptu.to_numpy(loss),
